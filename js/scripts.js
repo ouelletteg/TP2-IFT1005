f4fd8f6ebf5590511c3fd39f78284b8100fae788
ouelletteg <ouellettegenevieve@gmail.com>
2019-04-11 13:46:43 -0400

    Mise en forme

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,8 @@
  *
  * @param {*} data : Your aunt's data.
  */
-var dynamicActions = function(data) {
-  $(document).ready(function() {
+var dynamicActions = function (data) {
+  $(document).ready(function () {
     updateDocumentTitle();
     createArticles();
     createRecipes();
@@ -25,20 +25,20 @@ var dynamicActions = function(data) {
 
 
 var articlesPhotos = [
-['assets/trophé.png','Image by Quimono from pixabay'],
-['assets/VEGAN.png','Image by Gellinger from pixabay'],
-['assets/Passion.jpg','Image by Rawpixel from  pixabay']
+  ['assets/trophé.png', 'Image by Quimono from pixabay'],
+  ['assets/VEGAN.png', 'Image by Gellinger from pixabay'],
+  ['assets/Passion.jpg', 'Image by Rawpixel from  pixabay']
 ]
 var recettesPhotos = [
   ['assets/lasagna.jpg', 'Image by RitaE from Pixabay'],
   ['assets/tofu-tao.jpg', 'Image by Frank Zhang from Pixabay'],
-  ['assets/burger-vege.jpg','Image by Julia Nowak from Pixabay'],
+  ['assets/burger-vege.jpg', 'Image by Julia Nowak from Pixabay'],
   ['assets/mac-and-cheese.jpg', 'Photo by Hermes Rivera on Unsplash'],
   ['assets/saute-legumes.jpg', 'Photo by Clem Onojeghuo on Unsplash'],
   ['assets/salade-fruits.jpg', 'Image by silviarita from Pixabay'],
   ['assets/soupe-tofu.jpg', 'Image by jyleen21 from Pixabay'],
   ['assets/saumon-riz.jpg', 'Image by susumu kawakita from Pixabay'],
-  ['assets/quinoa-croquettes.jpg', 'Photo by ORNELLA BINNI on Unsplash'],
+  ['assets/quinoa-galettes2.jpg', 'Photo by Jessica Castro on Unsplash'],
   ['assets/poutine.jpg', 'Image by 0x010C on Wikipedia Commons.'],
   ['assets/tofu-beurre.jpg', 'Image by Devanath from Pixabay'],
   ['assets/boulettes_energie.jpg', 'Image by Karolina Grabowska from Pixabay']
@@ -51,36 +51,42 @@ var recettesPhotos = [
  * Update the document's title by using the provided data
  * from my aunt.
  */
-var updateDocumentTitle = function() {
+var updateDocumentTitle = function () {
   document.title = data.documentTitle;
 };
 
-var createArticles = function() {
-  Array.from($(".article h1")).forEach(function(titre, i) {
+var createArticles = function () {
+  Array.from($(".article h1")).forEach(function (titre, i) {
     titre.innerHTML = data.articles[i].title;
   });
-  Array.from($(".article h3")).forEach(function(sousTitre, i) {
+  Array.from($(".article h3")).forEach(function (sousTitre, i) {
     sousTitre.innerHTML = data.articles[i].subtitle;
   });
-  Array.from($(".article p")).forEach(function(texte, i) {
+  Array.from($(".article p")).forEach(function (texte, i) {
     texte.innerHTML = data.articles[i].content;
   });
 };
 //moi
 
-    
-    //
 
-var createRecipes = function() {
-  Array.from($("#recettes .card img")).forEach(function(image, i) {
-    image.src = recettesPhotos[i][0];
+//
+
+var createRecipes = function () {
+  Array.from($("#recettes .card img")).forEach(function (image, i) {
+    if (data.recipes[i].imgUrl == '') {
+      image.src = recettesPhotos[i][0];
+    } else {
+      image.src = data.recipes[i].imgUrl;
+    }
     image.alt = data.recipes[i].description;
-    image.title = recettesPhotos[i][1];
+    if (data.recipes[i].imgUrl == '') {
+      image.title = recettesPhotos[i][1];
+    }
   });
-  Array.from($(".card-title")).forEach(function(description, i) {
+  Array.from($(".card-title")).forEach(function (description, i) {
     description.innerHTML = data.recipes[i].description;
   });
-  Array.from($("#recettes .card p")).forEach(function(prix, i) {
+  Array.from($("#recettes .card p")).forEach(function (prix, i) {
     prix.innerHTML = data.recipes[i].price;
   });
 
