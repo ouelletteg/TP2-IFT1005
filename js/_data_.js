ea303cb24ece4440367819704a35f33ae4d3555b
Geneviève Ouellette <ouellettegenevieve@gmail.com>
2019-04-12 18:27:00 -0400

    Photos + Slick
    -Déplacement des tableaux de photos dans _data_.js
    
    -Création de fonctons pour inplémenter Slick

--- js/_data_.js
+++ js/_data_.js
@@ -5,8 +5,8 @@
 // Helpers
 // ======================================================
 
-var info = function(msg) { 
-  console.log("%c > "+msg, "color:#03a9f4;"); 
+var info = function(msg) {
+  console.log("%c > " + msg, "color:#03a9f4;");
 };
 
 // Local Storage
@@ -14,8 +14,8 @@ var info = function(msg) {
 
 // To save resources (time & computations), we will try to
 // retrieve the data from our local storage (if it exists).
-var storedData = localStorage.getItem('data');
-var data = storedData ? JSON.parse(storedData) : null; 
+var storedData = localStorage.getItem("data");
+var data = storedData ? JSON.parse(storedData) : null;
 
 // Fetch Data
 // ======================================================
@@ -23,22 +23,22 @@ var data = storedData ? JSON.parse(storedData) : null;
 var url = "https://api.jsonbin.io/b/5c951b129c83133c027b0810";
 
 /**
- * Fetch data from the above url, if we didn't found our  
+ * Fetch data from the above url, if we didn't found our
  * in the cookie object.
  */
-if(data === null) {
+if (data === null) {
   $.ajax({
     url: url,
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    dataType: 'json',
+    dataType: "json"
   })
     .done(function(result) {
       // Save data in global variable.
       data = result;
-      // Save data in the local storage to save resources 
+      // Save data in the local storage to save resources
       // on the next reload.
-      localStorage.setItem('data', JSON.stringify(result));
+      localStorage.setItem("data", JSON.stringify(result));
       info("SAVED DATA IN LOCAL STORAGE");
       // Execute dynamic actions to update the DOM.
       dynamicActions(data);
@@ -50,3 +50,27 @@ if(data === null) {
   info("DATA LOADED FROM LOCAL STORAGE");
   dynamicActions(data);
 }
+
+// Arrays of photos
+// ====================================================
+
+var articlesPhotos = [
+  ["assets/trophé.png", "Image by Quimono from pixabay"],
+  ["assets/VEGAN.png", "Image by Gellinger from pixabay"],
+  ["assets/Passion.jpg", "Image by Rawpixel from  pixabay"]
+];
+
+var recettesPhotos = [
+  ["assets/lasagna.jpg", "Image by RitaE from Pixabay"],
+  ["assets/tofu-tao.jpg", "Image by Frank Zhang from Pixabay"],
+  ["assets/burger-vege.jpg", "Image by Julia Nowak from Pixabay"],
+  ["assets/mac-and-cheese.jpg", "Photo by Hermes Rivera on Unsplash"],
+  ["assets/saute-legumes.jpg", "Photo by Clem Onojeghuo on Unsplash"],
+  ["assets/salade-fruits.jpg", "Image by silviarita from Pixabay"],
+  ["assets/soupe-tofu.jpg", "Image by jyleen21 from Pixabay"],
+  ["assets/saumon-riz.jpg", "Image by susumu kawakita from Pixabay"],
+  ["assets/quinoa-galettes2.jpg", "Photo by Jessica Castro on Unsplash"],
+  ["assets/poutine.jpg", "Image by 0x010C on Wikipedia Commons."],
+  ["assets/tofu-beurre.jpg", "Image by Devanath from Pixabay"],
+  ["assets/boulettes_energie.jpg", "Image by Karolina Grabowska from Pixabay"]
+];
--- js/scripts.js
+++ js/scripts.js
@@ -8,41 +8,19 @@
 /**
  * Execute all my functions.
  *
- * @param {*} data : Your aunt's data.
+ * @param {*} data : Your aunt's data. + Some photos
  */
 var dynamicActions = function(data) {
   $(document).ready(function() {
     updateNavbar();
     updateDocumentTitle();
     updateArticles();
+    slickUpArticles();
     createRecipes();
     updateRecipes();
     // etc.
   });
 };
-
-// Arrays of photos
-
-var articlesPhotos = [
-  ["assets/trophé.png", "Image by Quimono from pixabay"],
-  ["assets/VEGAN.png", "Image by Gellinger from pixabay"],
-  ["assets/Passion.jpg", "Image by Rawpixel from  pixabay"]
-];
-
-var recettesPhotos = [
-  ["assets/lasagna.jpg", "Image by RitaE from Pixabay"],
-  ["assets/tofu-tao.jpg", "Image by Frank Zhang from Pixabay"],
-  ["assets/burger-vege.jpg", "Image by Julia Nowak from Pixabay"],
-  ["assets/mac-and-cheese.jpg", "Photo by Hermes Rivera on Unsplash"],
-  ["assets/saute-legumes.jpg", "Photo by Clem Onojeghuo on Unsplash"],
-  ["assets/salade-fruits.jpg", "Image by silviarita from Pixabay"],
-  ["assets/soupe-tofu.jpg", "Image by jyleen21 from Pixabay"],
-  ["assets/saumon-riz.jpg", "Image by susumu kawakita from Pixabay"],
-  ["assets/quinoa-galettes2.jpg", "Photo by Jessica Castro on Unsplash"],
-  ["assets/poutine.jpg", "Image by 0x010C on Wikipedia Commons."],
-  ["assets/tofu-beurre.jpg", "Image by Devanath from Pixabay"],
-  ["assets/boulettes_energie.jpg", "Image by Karolina Grabowska from Pixabay"]
-];
 
 // My functions
 // ======================================================
@@ -62,10 +40,6 @@ var updateNavbar = function() {
 };
 
 var updateArticles = function() {
-  $("article").addClass("carousel-item");
-  $("article > div").addClass("row");
-  $("article > div > div").addClass("col d-none d-md-block");
-  $("article img").addClass("d-block w-100");
   Array.from($("article h1")).forEach(function(titre, i) {
     titre.innerHTML = data.articles[i].title;
   });
@@ -74,6 +48,16 @@ var updateArticles = function() {
   });
   Array.from($("article p")).forEach(function(texte, i) {
     texte.innerHTML = data.articles[i].content;
+  });
+};
+
+var slickUpArticles = function() {
+  $("#accueil").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1
   });
 };
 
@@ -89,9 +73,7 @@ var createRecipes = function() {
 };
 
 var updateRecipes = function() {
-  /*Mise des classes Bootstrap et des données JSON*/
-  $("#recettes > div").addClass("container");
-  $("#recettes > div > div").addClass("row");
+  /*Mise des classes Bootstrap et des données JSON des recttes*/
   $("#recettes > div > div > div").addClass("col-6 col-md-4 col-lg-3");
   $(".card img").addClass("card-img-top");
   $(".card div").addClass("card-body");
